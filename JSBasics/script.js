90c7bb2d54c1c7a425e1a7339da68ce550e92e5d
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-04-20 20:42:19 +0530

    feat: add new index.html and script.js files; implement basic HTML structure and JavaScript functionality

---
+++ JSBasics/script.js
@@ -0,0 +1,25 @@
+console.log("Hello world");
+console.log("Hello world 12123");
+
+
+
+// What is javascript
+// JavaScript is a dynamic typed language
+
+
+var x = 34;
+
+let a = 5;
+
+const pi = 3.14;
+
+let m = "0"
+let n = 10
+
+let mn = (m>n) ? "true" : "false"
+
+console.log(mn);
+
+console.log(m!=n);
+
+
