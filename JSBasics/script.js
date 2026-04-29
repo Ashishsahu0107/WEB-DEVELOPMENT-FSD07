7c431b77683cde1772124baaa6b65d9d54a46841
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-04-29 19:33:59 +0530

    fix: comment out console logs and unused variables for cleaner code

--- JSBasics/script.js
+++ JSBasics/script.js
@@ -1,5 +1,5 @@
-console.log("Hello world");
-console.log("Hello world 12123");
+// console.log("Hello world");
+// console.log("Hello world 12123");
 
 
 
@@ -7,19 +7,91 @@ console.log("Hello world 12123");
 // JavaScript is a dynamic typed language
 
 
-var x = 34;
+// var x = 34;
 
-let a = 5;
+// let a = 5;
 
-const pi = 3.14;
+// const pi = 3.14;
 
-let m = "0"
-let n = 10
+// let m = "0"
+// let n = 10
 
-let mn = (m>n) ? "true" : "false"
+// let mn = (m>n) ? "true" : "false"
 
-console.log(mn);
+// console.log(mn);
 
-console.log(m!=n);
+// console.log(m!=n);
+
+
+// null
+// undefined
+//
+
+
+
+// iteration statement
+
+// 1) Entry controlled -: indian railway
+// 2) Exit controlled -: metro - entry ke pahle ticket
+
+
+// loop -:
+
+// 1) initialization
+// 2) condition
+// 3) updation
+// 4) working
+
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(`${2} * ${i} = ${2*i}`);
+// }
+
+
+let arr = [24, 58, 33, 86, 96, 12, 53, 23, 32, 100];
+// for (let i = 0; i < 10; i++){
+//     console.log(arr[i]);
+// }
+
+// let i = 0;
+// while (i < 10) {
+//     console.log(arr[i]);
+//     i++;
+// }
+
+
+let i = 1;
+while (i < 10) {
+    console.log(`${2} * ${i} = ${2*i}`);
+    i++;
+}
+
+
+// let i = 0;
+// do {
+//     console.log(arr[i]);
+//     i++;
+// } while (i<10);
+
+
+let j = 1;
+do {
+    console.log(`${2} * ${j} = ${2*j}`);
+    j++;
+} while (j<10);
+
+
+
+/*
+
+i -> c -> w -> u
+
+i -> Initialization
+c -> Condition
+u -> Updation
+w -> Working
+
+
+*/
 
 
