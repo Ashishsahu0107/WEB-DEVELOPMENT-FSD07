a789828d55efa11717f85ce7e7e2314d02ec2421
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-04-30 19:48:51 +0530

    feat: remove old index.html and script.js files; add new index.html and script.js for condition and loop examples

---
+++ JSBasics/condition-statement/script.js
@@ -0,0 +1,117 @@
+// let num = prompt("Enter your number ");
+// if (num > 0) {
+//   console.log("positive number");
+// } else if (num < 0) {
+//   console.log("negative number");
+// } else {
+//   console.log("number is zero");
+// }
+
+
+// let age = 18;
+// if (age >= 18) {
+//     console.log("you can vote");
+// } else {
+//     console.log("you can nit vote");
+// }
+
+// let num = 10;
+// if (num % 2 == 0) {
+//     console.log("Even");
+
+// } else {
+//     console.log("Odd");
+
+// }
+
+
+
+// let marks = 10;
+// if (marks > 90) {
+//     console.log("A");
+// } else if (marks > 75 && marks <= 90) {
+//     console.log("B");
+// } else if (marks > 50 && marks <= 75) {
+//     console.log("C");
+// } else {
+//     console.log("Fail");
+// }
+
+
+// let username =prompt("Enter your email: ");
+
+// if (username === "admin") {
+//     console.log("email matched");
+//     let pass = prompt("Enter your password: ");
+//     if (pass === "123") {
+//         console.log("login successfull");
+//     } else {
+//         console.log("invilid password");
+//     }
+// } else {
+//     console.log("invilid email");
+// }
+
+
+// let data;
+// let name = true;
+
+// data = name || "N/A";
+// console.log(data);
+
+
+
+let choice = 200;
+switch (choice) {
+    case 1: {
+        console.log("Tea");
+        break;
+    }
+    case 2: {
+        console.log("Coffee");
+        break;
+    }
+    case 3: {
+        console.log("Lassi");
+        break;
+    }
+    default: {
+        console.log("Pani");
+        break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
