aaac9d514746c41e594e5f0cb57725384b22a7ad
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-05-01 18:45:43 +0530

    feat: add function.js and index.html files; implement basic function and HTML structure

--- JSBasics/condition-statement/script.js
+++ JSBasics/condition-statement/script.js
@@ -61,25 +61,40 @@
 
 
 
-let choice = 200;
-switch (choice) {
-    case 1: {
-        console.log("Tea");
-        break;
-    }
-    case 2: {
-        console.log("Coffee");
-        break;
-    }
-    case 3: {
-        console.log("Lassi");
-        break;
-    }
-    default: {
-        console.log("Pani");
-        break;
-    }
-}
+// let choice = 200;
+// switch (choice) {
+//     case 1: {
+//         console.log("Tea");
+//         break;
+//     }
+//     case 2: {
+//         console.log("Coffee");
+//         break;
+//     }
+//     case 3: {
+//         console.log("Lassi");
+//         break;
+//     }
+//     default: {
+//         console.log("Pani");
+//         break;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ JSBasics/function-method/function.js
@@ -0,0 +1,15 @@
+let a = 8;
+let b = 8;
+
+function sum(x, y){
+    let z = x + y;
+    console.log(z);
+}
+sum(a, b);
+
+
+// const sum = (a, b) => {
+//     return a + b;
+// }
+
+// console.log(sum(45,5));
