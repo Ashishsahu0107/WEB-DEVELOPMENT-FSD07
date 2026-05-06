fe0fa2fd89f02123fa43cab499447ae4becb563a
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-05-06 07:56:08 +0530

    feat: add arrayMethod.js and index.html files; implement array manipulation examples and logging

---
+++ JSBasics/array/arrayMethod.js
@@ -0,0 +1,26 @@
+// let arr = [12, 23, 65, 25, 45, 85, 5, 32];
+
+// arr.push(23);
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+// arr.unshift(23);
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+
+// console.log(arr.at(0));
+// console.log(arr.indexOf(85));
+// console.log(arr.includes(89));
+
+let arr = [5, 4, 3, 2, 4, 9, 5, 1];
+console.log(arr.sort());
+
+arr.forEach((ele) => {
+    console.log(ele.sort);
+    
+})
---
+++ JSBasics/array/script.js
@@ -0,0 +1,25 @@
+let arr = [12, 23, 65, 25, 45, 85, 5, 32];
+
+
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+    
+}
+
+console.log(arr);
+
+
+arr[2] = arr[2] + 10;
+console.log(arr);
+
+
+arr = [];
+
+console.log(arr);
+
+
+
+
+
