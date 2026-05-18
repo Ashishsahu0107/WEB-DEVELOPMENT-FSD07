8852efaf9062e25d2541e8deadee5b03e25cce7c
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-05-18 20:30:33 +0530

    feat: implement Password Manager with HTML, CSS, and JavaScript; add functionality to save and display passwords using localStorage

---
+++ AdvanceJavaScript/CallbackFunction/app.js
@@ -0,0 +1,61 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// show();
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+
+
+function show(value, nextData) {
+    setTimeout(() => {
+        console.log("I am test", value);
+
+        if (nextData) {
+            nextData();
+        }
+    }, 4000)
+}
+
+show("Prasoon", () => {
+    show("Anit", () => {
+        show("Ashish", () => {
+            show("Shubham", () => {
+                show("Priyanshu", () => {
+                    console.log("No Data Found");
+                })
+            })
+        })
+    });
+});
+
+
+
+
+
+
+
+// // server
+// function willDoSomthing(p, q, add) {
+//     console.log("Doing Something");
+
+//     let m = p + 10;
+//     let n = q + 10;
+
+//     console.log(add(m, n));
+// }
+
+// // Database
+// function sum(a, b) {
+//     let x = a + 1;
+//     let y = b - 1;
+
+//     return x + y;
+// }
+
+// willDoSomthing(4, 5, sum);
+
+
+
