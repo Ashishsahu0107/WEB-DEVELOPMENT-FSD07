660423d26bd686d953a450e8fb48343a409b7161
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-05-19 20:15:18 +0530

    feat: add async function to retrieve data with Promise handling

---
+++ AdvanceJavaScript/PromiseFunction/app.js
@@ -0,0 +1,31 @@
+async function getData(value) {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            if (value === 3) {
+                reject("Query not found")
+            }
+            else {
+                console.log("Data Sent", value);
+                resolve("Query solve")
+            }
+        }, 2000)
+    })
+}
+
+// const myPromise = getData(2);
+// console.log(myPromise);
+
+
+// getData(1)
+//     .then(() => getData(2))
+//     .then(() => getData(3))
+//     .then(() => getData(4))
+//     .catch((rej) => console.log(rej))
+
+
+await getData(1)
+await getData(2)
+await getData(3)
+await getData(4)
