1b36eff6dec547ec8dd33ea5bec388f427a23b4a
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-05-30 20:42:34 +0530

    feat: initialize myFirstReactApp with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development and build.
    - Created favicon.svg and icons.svg for application branding.
    - Implemented main application component (App.jsx) with a simple greeting.
    - Added global styles using Tailwind CSS in index.css.
    - Set up entry point (main.jsx) for rendering the application.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ frontend/myFirstReactApp/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='text-center'>
+        <h1 className='text-9xl text-red-500 capitalize font-bold'>Ashish sahu</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
