ec32b9a064ab9ba4af80f1ee6631a60877733cd2
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-06-12 20:46:07 +0530

    Add initial project structure with Navbar, Topbar, Home components and SVG assets
    - Created Navbar component with a heading and link
    - Added Topbar component placeholder
    - Implemented Home component with basic structure
    - Included react.svg and vite.svg assets
    - Set up main entry point in main.jsx
    - Imported Tailwind CSS in index.css

---
+++ Projects/nykaaMakeUp/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <div>
+                <h1>BEAUTY BONANZA Get Your Amazing Deals</h1>
+                <div>
+                    <Link to='/'>Get App</Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+//  http://makeup-api.herokuapp.com/api/v1/products
+
+export default Navbar
