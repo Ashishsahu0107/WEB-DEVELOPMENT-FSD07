e0966615bbac1944e3e410619865151c46e37cdc
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-06-12 20:39:34 +0530

    feat: add App component with routing and initial styles

---
+++ Projects/fakeStore/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
+
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product" element={<Products />} />
+          <Route path="/contact-us" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
