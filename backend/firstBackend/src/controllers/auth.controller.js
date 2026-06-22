bd71f49f853cf41697803c7cb34c29f76b510700
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-06-22 20:10:00 +0530

    feat: add authentication and public controllers for user login, registration, and contact

---
+++ backend/firstBackend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const LoginUser = (req, res) => {
+    res.json({message : "Login Successfull form controller"})
+};
+
+export const RegisterUser = (req, res) => {
+    res.json({message : "register Successfull form controller"})
+};
+
+export const LogoutUser = (req, res) => {
+    res.json({message : "Logout Successfull form controller"})
+};
