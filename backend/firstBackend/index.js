c2eea5de82b4d8e1dc459a9f5e070efa8368d7e4
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-06-22 19:17:19 +0530

    feat: implement authentication and public routes for the backend

--- backend/firstBackend/index.js
+++ backend/firstBackend/index.js
@@ -2,16 +2,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import AuthRouter from './src/routers/auth.route.js';
+import PublicRouter from './src/routers/public.route.js';
 
 const app = express(); // object
 
+
+app.use("/auth", AuthRouter);
+app.use("/public", PublicRouter);
+
+
+
+// Default APT
+
 app.get("/", (req, res) => {
     console.log("Server Started");
-    res.json({ message: "Welcome to mu first backend Projects" });
-})
-
-app.get("/login", (req, res) => {
-    res.json({message: "Login Successful"});
+    res.json({ message: "Welcome to my first backend Projects" });
 })
 
 
@@ -19,4 +25,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("Server Started on port : ",port);
 })
-3
