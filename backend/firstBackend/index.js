58e8aeea189119480cefa1347cc168831934932c
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-06-22 20:10:06 +0530

    feat: implement authentication routes and connect public contact route to controllers

--- backend/firstBackend/index.js
+++ backend/firstBackend/index.js
@@ -7,21 +7,16 @@ import PublicRouter from './src/routers/public.route.js';
 
 const app = express(); // object
 
-
 app.use("/auth", AuthRouter);
 app.use("/public", PublicRouter);
 
-
-
 // Default APT
-
 app.get("/", (req, res) => {
     console.log("Server Started");
     res.json({ message: "Welcome to my first backend Projects" });
 })
 
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log("Server Started on port : ",port);
+    console.log("Server Started on port : ", port);
 })
--- backend/firstBackend/src/routers/auth.route.js
+++ backend/firstBackend/src/routers/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { LoginUser, RegisterUser, LogoutUser } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.post("/login");
-router.post("/register");
-router.get("/logout");
+router.post("/login", LoginUser);
+router.post("/register", RegisterUser);
+router.post("/logout", LogoutUser);
 
 export default router;
